f36328f8b843c225b43562d58c009b456e7dfc7f
LE-HU <wojciechlech1337@gmail.com>
2021-01-25 20:25:55 +0100

    #list_all_views, #list_unique_views

--- ruby_app/parser.rb
+++ ruby_app/parser.rb
@@ -1,15 +1,27 @@
 # ruby 2.7.2
 
-class Parser
-  def initialize(argv)
-    @file = argv.first
+class LogParser
+  def initialize(file)
+    @log_data = File.read(file)
   end
 
-  def list_all_views; end
+  def list_all_views
+    @log_data.split("\n").each_with_object(Hash.new(0)) do |line, hash|
+      path, ip = line.split(' ')
+      hash[path] += 1
+    end
+  end
 
-  def list_unique_views; end
+  def list_unique_views
+    views = @log_data.split("\n").each_with_object({}) do |line, hash|
+      path, ip = line.split(' ')
+      hash[path] ||= []
+      hash[path] << ip unless hash[path].include? ip
+    end
+    views.each { |k, v| views[k] = v.size }
+  end
 end
 
-x = Parser.new(ARGV)
-# puts ARGV.first
-puts x.instance_variable_get('@file')
+x = LogParser.new(ARGV[0])
+pp x.list_all_views
+pp x.list_unique_views
