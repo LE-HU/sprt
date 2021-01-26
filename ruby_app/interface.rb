6e59130976174cb92dffd34dd8a83f5c3e2e4424
LE-HU <wojciechlech1337@gmail.com>
2021-01-26 20:46:05 +0100

    add -h help flag to display CLI help

--- ruby_app/interface.rb
+++ ruby_app/interface.rb
@@ -2,16 +2,23 @@ class Interface
   def self.get_opt(args)
     options = {}
     opts = OptionParser.new do |opts|
-      opts.on('-f', '--file FILE' 'Log file') do |file|
+      opts.banner = 'Usage: parser.rb [options]'
+
+      opts.on('-f', '--file FILE', 'Specify log file') do |file|
         options[:file] = file
       end
 
-      opts.on('-a', 'List all views') do
+      opts.on('-a', '--all', 'List all views') do
         options[:all] = true
       end
 
-      opts.on('-u', 'List unique views') do
+      opts.on('-u', '--uniq', 'List unique views') do
         options[:uniq] = true
+      end
+
+      opts.on('-h', '--help', 'Prints this help') do
+        puts opts
+        exit
       end
     end.parse(args)
 
